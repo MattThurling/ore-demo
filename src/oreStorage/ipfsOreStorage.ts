8e93c45fe19150a429dda428094ca3d9e90a725d
Matt Thurling <mthurling@icloud.com>
2025-12-12 10:19:58 +0000

    publish

--- src/oreStorage/ipfsOreStorage.ts
+++ src/oreStorage/ipfsOreStorage.ts
@@ -1,31 +1,7 @@
 import type oreStorage from './oreStorageInterface'
-import { toArrayBuffer, type OreTrackManifest } from '@mattthurling/ore'
 
 export default class IpfsOreStorage implements oreStorage {
-  async write(manifest: OreTrackManifest, data:Uint8Array): Promise<string | void> {
-    const encFormData = new FormData()
-    const encBlob = new Blob([toArrayBuffer(data)], { type: 'application/octet-stream' })
-    encFormData.append('file', encBlob, manifest.meta!.title)
-    const encCid = await this._writeIpfs(encFormData)
-
-    const publishMetadata = {
-      cid: encCid,
-      manifest,
-    }
-
-    const metaJson = JSON.stringify(publishMetadata, null, 2)
-
-    const metaFormData = new FormData()
-    const metaBlob = new Blob([metaJson], { type: 'application/json'})
-    metaFormData.append('file', metaBlob, 'not sure it matters')
-
-    const metaCid = await this._writeIpfs(metaFormData)
-    return metaCid
-
-  }
-
-  // Write the encrypted file to IPFS
-  async _writeIpfs(formData: FormData): Promise<string | void> {
+  async write(formData: FormData): Promise<string | void> {
     try {
       const response = await fetch(`${import.meta.env.VITE_STORAGE_URL}/pin` , {
         method: 'POST',
--- src/routes/Publish.tsx
+++ src/routes/Publish.tsx
@@ -5,6 +5,7 @@ import {
   importAesGcmKeyFromBytes,
   encryptBytesAesGcm,
   createTrackManifest,
+  toArrayBuffer,
 } from '@mattthurling/ore'
 import { downloadBlob } from '../utils/os'
 
@@ -12,8 +13,12 @@ const oreStorage = createStorage(import.meta.env.VITE_STORAGE_TYPE)
 
 function Publish() {
   const [publishFile, setPublishFile] = useState<File | null>(null)
+  const [trackName, setTrackName] = useState<string>('')
+  const [publishId, setPublishId] = useState<string>('')
+  const [status, setStatus] = useState<'' | 'uploading' | 'published'>('')
 
   async function encryptAndPublish() {
+    setStatus('uploading')
     // Turn the uploaded file into bytes
     const bytes = new Uint8Array(await publishFile!.arrayBuffer())
     // Generate a key for encryption
@@ -21,57 +26,103 @@ function Publish() {
     const aesKey = await importAesGcmKeyFromBytes(contentKey)
     // Encrypt the track
     const { ciphertext, iv } = await encryptBytesAesGcm(bytes, aesKey)
+    // Wrap it in a FormData object
+    const encFormData = new FormData()
+    const encBlob = new Blob([toArrayBuffer(ciphertext)], { type: 'application/octet-stream' })
+    encFormData.append('file', encBlob, publishFile!.name)
+    // Write to specified storage and get an 'id'
+    const encId = await oreStorage!.write(encFormData)
     // Create track manifest
     const manifest = createTrackManifest({
       mimeType: publishFile!.type || 'audio/mpeg',
       sizeBytes: bytes.length,
       iv,
       meta: {
-        title: publishFile!.name
+        title: trackName,
+        fileName: publishFile!.name,
       }
     })
-    console.log(manifest)
-    // Write to specified storage
-    const publishId = await oreStorage!.write(manifest, ciphertext)
+    // Add the id of the encrypted file
+    const publishMeta = {
+      cid:encId,
+      manifest,
+    }
+    // Now wrap this in a new FormDataObject
+    const publishFormData = new FormData()
+    const publishJson = JSON.stringify(publishMeta, null, 2)
+    const publishBlob = new Blob([publishJson], { type: 'application/json'})
+    publishFormData.append('file', publishBlob, trackName)
+    // And publish this as the reference to to the encrypted file
+    const pId = await oreStorage!.write(publishFormData)
+    // Now write a local publish record, including the content key used for encryption
+    if (pId) setPublishId(pId); else return
     
     const publishRecord = {
-      cid: publishId,
-      url: `https://ipfs.io/ipfs/${publishId}`,
+      cid: pId,
+      url: `https://ipfs.io/ipfs/${pId}`,
       key: btoa(String.fromCharCode(...contentKey)),
     }
 
     const json = JSON.stringify(publishRecord, null, 2)
     const jsonBlob = new Blob([json], { type: 'application/json'})
-
-    downloadBlob(jsonBlob, publishId + '.json') 
+    // Save, giving it a nicer filename
+    downloadBlob(jsonBlob, trackName + '-' + pId.slice(0,4) + '...' + pId.slice(-4) + '.json') 
   }
 
 
   return (
     <div className='p-8'>
       <p className='text'>
-        Choose a .wav or .mp3 file to publish. It will be encrypted once, and the (symmetric) encryption key will be saved (locally, for now) along with the encrypted file.
+        Choose a .wav or .mp3 file to publish. It will be encrypted and published to IPFS. The (symmetric) encryption key will be saved locally.
       </p>
       <div className='divider'></div>
-      <div className='md:flex'>
-        <div>
+      <div className='md:flex md:gap-6'>
+        <div className='md:flex-1'>
           <input
-            className='file-input'
+            className='file-input w-full'
             type='file'
             accept='audio/*'
             onChange={e => setPublishFile(e.target.files?.[0] ?? null)}
           />
         </div>
-        <div className='mt-6 md:mt-0 md:pl-8'>
+        <div className='mt-6 md:mt-0 md:flex-1'>
+          <input
+            className='input w-full'
+            type='text'
+            onChange={e => setTrackName(e.target.value)}
+            placeholder='Enter track name' />
+        </div>
+        <div className='mt-6 md:mt-0 md:flex-1'>
           <button
-            className='btn btn-secondary min-w-64'
-            disabled={!publishFile}
+            className='btn btn-secondary w-full'
+            disabled={!publishFile || !trackName || !!status}
             onClick={encryptAndPublish}
             >
               Encrypt and publish
           </button>
         </div>
       </div>
+      {publishId && (
+        <div>
+          <div className='divider'></div>
+          <p className='text'>
+            Your track has been published to the InterPlanetary File System!
+          </p>
+          <div className='mt-6'>
+            <a href={`https://ipfs.io/ipfs/${publishId}`} className='hover-3d mx-2 cursor-pointer'>
+                <div className='card w-full bg-black text-white'>
+                  <div className='card-body'>
+                    <div className='card-title'>
+                      {trackName}
+                    </div>
+                    <p>{publishId}</p>
+                  </div>
+                  
+                </div>
+            </a>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
